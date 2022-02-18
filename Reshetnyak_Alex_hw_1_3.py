a8ffd9ad754b9fc070adc40a993e452c8d915548
reshetnyak <reshetnayk.aa@gmail.com>
2022-02-18 23:54:21 +0500

    first hw

---
+++ Reshetnyak_Alex_hw_1_3.py
@@ -0,0 +1,14 @@
+a = ("Процент")
+b = ("Процента")
+c = ("Процентов")
+numbs = [11,12,13,14]
+for i in range(100):
+    i = i + 1
+    if i in numbs:
+        print(i, "процентов")
+    elif i % 10 == 1:
+        print(i, "процент")
+    elif i % 10 > 1 and i % 10 <5:
+        print(i, "процента")
+    else:
+        print(i, "процентов")
